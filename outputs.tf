7813e3d7b00fddd7c62f22b8a0fb3e82565bf62c
Mauro Solidoro <mauro.solidoro@outlook.it>
2024-07-24 12:42:57 +0200

    feat: add bastion host outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "security_group_id" {
   value       = aws_security_group.this.id
   description = "ID of the security group assigned to the bastion host."
 }
+
+output "bastion_iam_role_arn" {
+  value = length(module.instance_profile_role) > 0 ? module.instance_profile_role[0].iam_role_arn : null
+}
+
+output "bastion_iam_role_name" {
+  value = length(module.instance_profile_role) > 0 ? module.instance_profile_role[0].iam_role_name : null
+}
